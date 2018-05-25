780cf14476c4a2e06815bad221782849aaf278ca
Osvaldo Santana Neto <osantana@osantana.me>
2018-05-24 23:58:39 -0300

    Add a ctypes usage sample

---
+++ go_version/gosumctypes.py
@@ -0,0 +1,20 @@
+from ctypes import POINTER, Structure, c_longlong, c_void_p, cdll
+
+
+class GoSlice(Structure):
+    _fields_ = [("data", POINTER(c_void_p)),
+                ("len", c_longlong), ("cap", c_longlong)]
+
+def List(seq):
+    size = len(seq)
+    return GoSlice((c_void_p * size)(*seq), size, size)
+
+
+gosum = cdll.LoadLibrary("./gosum.so")
+gosum.SumSlice.argtypes = [GoSlice]
+gosum.SumSlice.restype = c_longlong
+
+numbers = range(0, 100)
+
+def test_golang_sum(benchmark):
+    benchmark(gosum.SumSlice, to_slice(numbers))
