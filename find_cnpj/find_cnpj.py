8fb5b7302cf9d9ca9c88a196ebdf2ebda1593d37
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-09-12 22:02:13 -0300

    Refactor find_cnpj.py

--- find_cnpj/find_cnpj.py
+++ find_cnpj/find_cnpj.py
@@ -1,40 +1,32 @@
 import re
 
-# Real data about CNPJ - too slow, too large and not versioned
-#
-# with open('./data/F.K03200UF.D71214SP', 'r', encoding='iso8859') as fp:
-#     content = fp.read()
-
-with open('./data/MINIMAL', 'r', encoding='iso8859') as fp:
-    content = fp.read()
-
-
-company_name = 'CARGOBR INTERMEDIACAO E AGENCIAMENTO DE NEGOCIOS S/A'
-expression = r'\d{{2}}(\d{{14}}).*{}.*'.format(company_name)
-pattern = re.compile(expression)
-
 
 def find_cnpj_using_in(content, company_name):
     return next(line for line in content.split('\n') if company_name in line)
 
 
 def find_cnpj_using_search(content, company_name):
+    expression = r'\d{{2}}(\d{{14}}).*{}.*'.format(company_name)
     return re.search(expression, content).group(1)
 
 
 def find_cnpj_using_findall(content, company_name):
+    expression = r'\d{{2}}(\d{{14}}).*{}.*'.format(company_name)
+    pattern = re.compile(expression)
     return pattern.findall(content)
 
 
-def test_find_cnpj_using_search(benchmark):
-    benchmark(find_cnpj_using_search, content, company_name)
-
-
-def test_find_cnpj_using_findall(benchmark):
-    benchmark(find_cnpj_using_findall, content, company_name)
-
-
 if __name__ == '__main__':
+    # Real data about CNPJ - too slow, too large and not versioned
+    #
+    # with open('./data/F.K03200UF.D71214SP', 'r', encoding='iso8859') as fp:
+    #     content = fp.read()
+
+    with open('./data/MINIMAL', 'r', encoding='iso8859') as fp:
+        content = fp.read()
+
+    company_name = 'CARGOBR INTERMEDIACAO E AGENCIAMENTO DE NEGOCIOS S/A'
+
     print(find_cnpj_using_in(content, company_name))
     print(find_cnpj_using_search(content, company_name))
     print(find_cnpj_using_findall(content, company_name))
