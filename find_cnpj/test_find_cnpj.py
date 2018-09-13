ac5cca822f4b9c3b42885be96aac8499dd6574be
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-09-12 23:02:40 -0300

    Add test to gofindcnpj.FindCnpjByContains

--- find_cnpj/test_find_cnpj.py
+++ find_cnpj/test_find_cnpj.py
@@ -1,3 +1,5 @@
+from ctypes import Structure, c_char_p, c_longlong, cdll
+
 import pytest
 
 from find_cnpj import (
@@ -5,6 +7,15 @@ from find_cnpj import (
     find_cnpj_using_in,
     find_cnpj_using_search,
 )
+
+
+class GoString(Structure):
+    _fields_ = [("p", c_char_p), ("n", c_longlong)]
+
+
+gofindcnpj = cdll.LoadLibrary("./gofindcnpj.so")
+gofindcnpj.FindCnpjByContains.argtypes = [GoString, GoString]
+gofindcnpj.FindCnpjByContains.restype = c_char_p
 
 
 @pytest.fixture
@@ -41,3 +52,16 @@ def test_find_cnpj_using_search_result(content, company_name):
 
 def test_find_cnpj_using_findall_result(content, company_name):
     assert find_cnpj_using_findall(content, company_name) == ['17781400000184']
+
+
+def test_golang_ctypes_find_cnpj_contains(content, company_name):
+    new_content = bytes(content, 'utf-8')
+    new_company_name = bytes(company_name, 'utf-8')
+    expected = b'0117781400000184CARGOBR INTERMEDIACAO E AGENCIAMENTO DE NEGOCIOS S/A'
+
+    result = gofindcnpj.FindCnpjByContains(
+        GoString(new_content, len(content)),
+        GoString(new_company_name, len(company_name)),
+    )
+
+    assert expected in result
