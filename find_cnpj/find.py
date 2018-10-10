5e7a082bcd1af6d4ec29bacb0b90b2d4c4899bcb
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-10-09 21:27:04 -0300

    Refactor find Python version

--- find_cnpj/find.py
+++ find_cnpj/find.py
@@ -2,7 +2,9 @@ import re
 
 
 def find_cnpj_using_in(content, company_name):
-    return next(line for line in content.split('\n') if company_name in line)
+    line = next(line for line in content.split('\n') if company_name in line)
+    line = int(line[2:16])
+    return line
 
 
 def find_cnpj_using_search(content, company_name):
@@ -13,20 +15,20 @@ def find_cnpj_using_search(content, company_name):
 def find_cnpj_using_findall(content, company_name):
     expression = r'\d{{2}}(\d{{14}}).*{}.*'.format(company_name)
     pattern = re.compile(expression)
-    return pattern.findall(content)
+    return pattern.findall(content)[0]
 
 
 if __name__ == '__main__':
     # Real data about CNPJ - too slow, too large and not versioned
     #
-    # with open('./data/F.K03200UF.D71214SP', 'r', encoding='iso8859') as fp:
+    # with open('./data/F.K03200UF.D71214PR', 'r', encoding='iso8859') as fp:
     #     content = fp.read()
 
     with open('./data/MINIMAL', 'r', encoding='iso8859') as fp:
         content = fp.read()
 
-    company_name = 'CARGOBR INTERMEDIACAO E AGENCIAMENTO DE NEGOCIOS S/A'
+    company_name = 'OLIST SERVICOS DIGITAIS LTDA'
 
-    print(find_cnpj_using_in(content, company_name))
-    print(find_cnpj_using_search(content, company_name))
-    print(find_cnpj_using_findall(content, company_name))
+    print("find_cnpj_using_in result: ", find_cnpj_using_in(content, company_name))
+    print("find_cnpj_using_search result: ", find_cnpj_using_search(content, company_name))
+    print("find_cnpj_using_findall result: ", find_cnpj_using_findall(content, company_name))
